25e3fc2484430868f8e981c14f6d3b81b3ae8170
Daniel Hynd <daniel.hynd@oxfordcc.co.uk>
2022-01-13 10:37:43 +0000

    Add toggle for local SMTP

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -131,8 +131,14 @@ DEFAULT_FROM_EMAIL = 'daniel.hynd@oxfordcc.co.uk'
 
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
-EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+if os.getenv('USE_LOCAL_SMTP'):
+    EMAIL_HOST = 'localhost'
+    EMAIL_PORT = 25
+    EMAIL_USE_TLS = False
+    print("Using local SMTP")
+else:
+    EMAIL_HOST = 'smtp.sendgrid.net'
+    EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
+    EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
