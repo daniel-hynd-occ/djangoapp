2c8f69de1352b2178b9b75c31156292269121892
Daniel Hynd <daniel.hynd@oxfordcc.co.uk>
2022-01-20 10:43:00 +0000

    Require unique email for accounts

--- azuresite/urls.py
+++ azuresite/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from registration.forms import RegistrationFormUniqueEmail
+from registration.backends.default.views import RegistrationView
 
 urlpatterns = [
     path('', include('projects.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/register/', RegistrationView.as_view(form_class=RegistrationFormUniqueEmail), name='registration_register'),
     path('accounts/', include('registration.backends.default.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('projects/', include('projects.urls'))
